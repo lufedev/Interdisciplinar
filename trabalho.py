5f8cc085e7d45e515d89eafff7f6528043752d6f
feluipe <felipeluizsantos.k@gmail.com>
2021-05-24 20:30:04 -0300

    regressão refeita

--- trabalho.py
+++ trabalho.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn import linear_model
 
 viagens = pd.read_csv('viagens2.csv', sep=';')
 del viagens['Unnamed: 5']
@@ -11,32 +12,19 @@ def correlacao(c1,c2):
 
     print("\nValor de correlação: ",viagens[nome].corr(viagens[nome2]))
 
-def regressao(c1,c2):
+def regressao(c1,c2,c3):
     nome  = viagens.columns[c1]
     nome2 = viagens.columns[c2]
+    nome3 = viagens.columns[c3]
+    X = viagens[[nome,nome2]]
+    Y = viagens[nome3]
 
-    # Set the features of our model, these are our potential inputs
-
-    valores = [nome,nome2]
-
-    # Set the variable X to be all our input columns: Temperature, Wind Speed and Pressure
-
-    X = viagens[valores]
-
-    # set y to be our output column: Humidity
-
-    y = viagens['Outros Gastos']
-
-    # plt.subplot enables us to plot mutliple graphs
-    # we produce scatter plots for Humidity against each of our input variables
-
-    plt.subplot(2,2,1)
-    plt.plot(X[nome],y)
-    plt.subplot(2,2,2)
-    plt.plot(X[nome2],y)
-    plt.show()
-    
-   
+    regr = linear_model.LinearRegression()
+    regr.fit(X, Y)
+    #qual é a média pro "nome3" quando o "nome" e o "nome2" são = 1000
+    pred = regr.predict([[1000, 1000]])
+    print(regr.coef_) 
+    print(pred) 
 
 
 for col in range(1,len(viagens.columns)):
@@ -44,5 +32,9 @@ for col in range(1,len(viagens.columns)):
 
 c1 = int(input("\nInsira o número da coluna 1: "))
 c2 = int(input("Insira o número da coluna 2: "))
-regressao(c1,c2)
+correlacao(c1,c2)
+c1 = int(input("\nInsira o número da coluna 1: "))
+c2 = int(input("Insira o número da coluna 2: "))
+c3 = int(input("Insira o número da coluna 3: "))
+regressao(c1,c2,c3)
 
