25b9cd19e0443dad761588732a3a1a9669e51fcd
feluipe <felipeluizsantos.k@gmail.com>
2021-06-09 21:35:17 -0300

    Trabalho funcionando direitinho

--- trabalho.py
+++ trabalho.py
@@ -3,8 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import linear_model
+from pathlib import Path
 
-lstViagens = pd.read_csv('viagens.csv', sep=';')
+lstViagens = pd.read_csv('a.csv', sep=';')
 del lstViagens['Unnamed: 5']
 
 
@@ -12,13 +13,25 @@ def correlacao(column1, column2):
     column1_name = lstViagens.columns[column1]
     column2_name = lstViagens.columns[column2]
 
-    correlation = lstViagens[column1_name].corr(lstViagens[column2_name])
+    correlation = lstViagens[column1_name].corr(lstViagens[column2_name],method='pearson')
 
     print(f"\nValor de correlação: { correlation }")
 
+    
+    plt.figure(figsize = (16,8))
+    plt.scatter(
+        lstViagens[column1_name], 
+        lstViagens[column2_name], 
+        c='red')
+    plt.xlabel(column1_name)
+    plt.ylabel(column2_name)
+    plt.show()
+
+
+
 
 def regressao(columnsX, columnY):
-    lstColumnsX = columnsX.split(" ")
+    lstColumnsX = columnsX.split(",")
     lstColumnNamesX = []
 
     for column in lstColumnsX:
@@ -34,15 +47,48 @@ def regressao(columnsX, columnY):
     coeficientRegression = regression.coef_
     score = regression.score(x, y)
 
-    # print(intercept, coeficientRegression, score)
+    #print(intercept, coeficientRegression, score)
 
     predictionColumns = []
-    print("\nValores para predição:\n")
-    
-    for columnName in lstColumnNamesX:
-        predictionColumns.append(float(input(columnName + ": ")))
+    print("\n=============Valores para predição=============\n")
+
+    tem_arquivo = input('Você possui um arquivo (S/N): ')
+
+    while (tem_arquivo not in ['S','N']):
+        tem_arquivo = input('Caractere inválido, escreva novamente!! (S/N): ')
+
+    while(tem_arquivo == 'S'): #Voce possui um arquivo (S/N):
         
-    #qual é a média pro "nome3" quando o "nome" e o "nome2" são = 1000
+        nome_arq = input('Digite o nome do arquivo (Sem o ".csv"): ')+'.csv'
+
+        #Se você tem o arquivo ele existe?
+        #Se sim:
+        if Path(nome_arq).is_file():
+
+            lstvalores = pd.read_csv(nome_arq, sep=',', header=None)
+            for i in range(0,3):
+                predictionColumns.append(float(lstvalores[i]))
+            break        
+            #Se não:
+
+        tem_arquivo = input('Arquivo não encontrado, deseja tentar de novo? (S/N): ')
+        while (tem_arquivo not in ['S','N']):
+            tem_arquivo = input('Caractere inválido, escreva novamente!! (S/N): ')
+        
+            
+
+    #Se não
+    if tem_arquivo == 'N' :
+        print("Digite os valores manualmente: ")
+        for columnName in lstColumnNamesX:
+            predictionColumns.append(float(input(columnName + ": ")))    
+                
+            
+  
+
+
+   
+   
     prediction = regression.predict([predictionColumns])
   
   
@@ -87,17 +133,41 @@ def menu():
 
     if option == 2:
         cls()
-        
+        flag = False
         columns = columnNames()
        
         [print("{}. {}".format(column, columns[column])) for column in columns]
 
-        columnsX = input("\nEscolha as colunas para x: ")
-       
-        for column in columnsX.split(" "):
+        columnsX = input("\nEscolha as colunas para x separadas por virgula: ")
+
+        
+        while (flag == False):
+
+            if (len(columnsX.split(",")) > 3 or len(columnsX.split(",")) < 2):
+                print("Quantidade de colunas invalidas, minimo de 2 colunas maximo de 3")
+                flag = False
+
+            else:
+                flag = True
+            
+            if flag == True:
+                for column in columnsX.split(","):
+                    if int(column) not in [1,2,3,4]:
+                        flag = False
+                        print("Coluna com valor invalido, digite novamente!")
+                        break
+                    flag = True
+
+            if flag == False:
+                columnsX = input("\nEscolha novamente as colunas para x separadas por virgula: ")
+                
+           
+           
+        for column in columnsX.split(","):
             del columns[int(column)]
     
-        if(len(columnsX.split(" ")) == 3):
+
+        if(len(columnsX.split(",")) == 3):
             regressao(columnsX, list(columns.values())[0])
         else:
             [print("{}. {}".format(column, columns[column])) for column in columns]
